85c4b2a929ccf70d9782b86659026a65b1a02a3e
aleks2414 <alejandroromo14@gmail.com>
2017-04-27 11:42:35 -0500

    brose elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-b03= User.create!({:email => "alex@brose.com", :password => "brose2017", :password_confirmation => "brose2017", :admin => true })
+b03= User.create!({:email => "gabriela.morales@brose.com", :password => "brose2017", :password_confirmation => "brose2017", :admin => true })
+b02= User.create!({:email => "marco.moreno@brose.com", :password => "brose2017", :password_confirmation => "brose2017", :admin => true })
+b04= User.create!({:email => "karina.martinez@brose.com", :password => "brose2017", :password_confirmation => "brose2017", :admin => true })
